ef78895a565324a8771080419aa83b9c985bbbed
churrundo <churrundo@gmail.com>
2023-08-28 19:18:06 -0600

    begin building pages

---
+++ src/pages/PollsPage/PollsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PollsPage.css';
+
+function PollsPage() {
+    return (
+        <div className="polls">
+            <h1>Ongoing Polls</h1>
+            {/* List or grid of ongoing polls */}
+        </div>
+    );
+}
+
+export default PollsPage;
