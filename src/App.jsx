c0287aede2eeab83fdc74d33ac663219941acce5
churrundo <churrundo@gmail.com>
2023-08-30 20:49:00 -0600

    add pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import HomePage from "./pages/HomePage/HomePage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import ComplaintsPage from "./pages/ComplaintsPage/ComplaintsPage";
+import AnnouncementsPage from "./pages/AnnouncementsPage/AnnouncementsPage";
+import PollsPage from "./pages/PollsPage/PollsPage";
 
 import Navbar from "./components/Navbar/Navbar";
 import IsPrivate from "./components/IsPrivate/IsPrivate";
@@ -18,7 +22,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-
         <Route
           path="/profile"
           element={
@@ -27,7 +30,6 @@ function App() {
             </IsPrivate>
           }
         />
-
         <Route
           path="/signup"
           element={
@@ -44,6 +46,38 @@ function App() {
             </IsAnon>
           }
         />
+        <Route
+          path="/dashboard"
+          element={
+            <IsPrivate>
+              <DashboardPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/complaints"
+          element={
+            <IsPrivate>
+              <ComplaintsPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/announcements"
+          element={
+            <IsPrivate>
+              <AnnouncementsPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/polls"
+          element={
+            <IsPrivate>
+              <PollsPage />
+            </IsPrivate>
+          }
+        />{" "}
       </Routes>
     </div>
   );
