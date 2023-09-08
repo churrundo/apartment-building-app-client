eae98597005522da99278502f659d231b6d54622
churrundo <churrundo@gmail.com>
2023-09-08 13:09:44 -0600

    conditionally render dashboard

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,29 +1,71 @@
-import React, { useContext } from 'react';
-import './DashboardPage.css';
-import { AuthContext } from "../../context/auth.context"
+import React, { useState, useEffect, useContext } from "react";
+import "./DashboardPage.css";
+import { AuthContext } from "../../context/auth.context";
+import AnnouncementService from "../../services/announcements.service";
+import PollService from "../../services/polls.service";
 
 function DashboardPage() {
-    const { user } = useContext(AuthContext);
-    return (
-        <div className="dashboard">
-            <div className="dashboard-header">
-                Welcome, {user.name}!
-            </div>
+  const { user } = useContext(AuthContext);
+  const [announcements, setAnnouncements] = useState([]);
+  const [polls, setPolls] = useState([]);
+  const [hasBuilding, setHasBuilding] = useState(false);
 
-            <div className="dashboard-content">
-                <section className="recent-announcements">
-                    <h2>Recent Announcements</h2>
-                    <a href='/announcements'>see all</a>
-                    {/* List of recent announcements */}
-                </section>
-                <section className="ongoing-polls">
-                    <h2>Ongoing Polls</h2>
-                    <a href='/polls'>see all</a>
-                    {/* List of ongoing polls */}
-                </section>
-            </div>
+  useEffect(() => {
+    if (user.buildingId) {
+      setHasBuilding(true);
+
+      // Fetch announcements based on user's building
+      AnnouncementService.getAnnouncementsByBuilding(user.buildingId)
+        .then((data) => {
+          setAnnouncements(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching announcements:", error);
+        });
+
+      // Fetch polls based on user's building
+      PollService.getPollsByBuilding(user.buildingId)
+        .then((data) => {
+          setPolls(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching polls:", error);
+        });
+    }
+  }, [user]);
+
+  return (
+    <div className="dashboard">
+      <div className="dashboard-header">Welcome, {user.name}!</div>
+
+      {hasBuilding ? (
+        <div className="dashboard-content">
+          <section className="recent-announcements">
+            <h2>Recent Announcements</h2>
+            {/* Display announcements */}
+            {announcements.map((announcement) => (
+              <div key={announcement.id}>{announcement.title}</div>
+            ))}
+            <a href="/announcements">see all</a>
+          </section>
+          <section className="ongoing-polls">
+            <h2>Ongoing Polls</h2>
+            {/* Display polls */}
+            {polls.map((poll) => (
+              <div key={poll.id}>{poll.title}</div>
+            ))}
+            <a href="/polls">see all</a>
+          </section>
         </div>
-    );
+      ) : (
+        <div className="no-building-notice">
+          You're not associated with any building.{" "}
+          <a href="/join-building">Join an existing building</a> or{" "}
+          <a href="/create-building">create a new one</a>.
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default DashboardPage;
