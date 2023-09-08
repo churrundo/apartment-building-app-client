ce3b48d293b51ec1798cfa68064992f336bcd37d
churrundo <churrundo@gmail.com>
2023-09-08 15:51:38 -0600

    add building reference to poll and announcement service

--- src/services/announcements.service.js
+++ src/services/announcements.service.js
@@ -1,8 +1,10 @@
 import service from './index';
 
 const announcementService = {
-    getAllAnnouncements : ()=> service.get('/api/announcements'),
+    
     createAnnouncement : (formData)=> service.post('/api/announcements', formData),
+    getAllAnnouncements : ()=> service.get('/api/announcements'),
+    getAnnouncementsByBuilding: (buildingId)=> service.get(`/api/announcements/buildingId?:${buildingId}`),
     getAnnouncement: (announcementId) => service.get(`/api/announcements/${announcementId}`),
     updateAnnouncement: (announcementId, formData) => service.put(`/api/announcements/${announcementId}`, formData),
     deleteAnnouncement: (announcementId) => service.delete(`/api/announcements/${announcementId}`),
