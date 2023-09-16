8402d889cc782253f990ac7ecb2e1308f2ae4c36
churrundo <churrundo@gmail.com>
2023-09-15 19:37:47 -0600

    final touch

--- src/services/polls.service.js
+++ src/services/polls.service.js
@@ -1,14 +1,18 @@
-import service from './index';
+import service from "./index";
 
 const pollService = {
-    createPoll : (formData)=> service.post('/api/polls', formData),
-    getAllPolls : ()=> service.get('/api/polls'),
-    getPollsByBuilding: (buildingId) => service.get(`/api/polls/building/${buildingId}`),
-    getPoll: (pollId) => service.get(`/api/polls/${pollId}`),
-    updatePoll: (pollId, formData) => service.put(`/api/polls/${pollId}`, formData),
-    vote: (pollId, optionId, userId) => service.post(`/api/polls/${pollId}/vote`, { optionId, userId }),
-    closePoll: (pollId, userId) => service.put(`/api/polls/${pollId}/close`, { userId }),
-    deletePoll: (pollId) => service.delete(`/api/polls/${pollId}`),
-}
+  createPoll: (formData) => service.post("/api/polls", formData),
+  getAllPolls: () => service.get("/api/polls"),
+  getPollsByBuilding: (buildingId) =>
+    service.get(`/api/polls?buildingId=${buildingId}`),
+  getPoll: (pollId) => service.get(`/api/polls/${pollId}`),
+  updatePoll: (pollId, formData) =>
+    service.put(`/api/polls/${pollId}`, formData),
+  vote: (pollId, optionId, userId) =>
+    service.post(`/api/polls/${pollId}/vote`, { optionId, userId }),
+  closePoll: (pollId, userId) =>
+    service.put(`/api/polls/${pollId}/close`, { userId }),
+  deletePoll: (pollId) => service.delete(`/api/polls/${pollId}`),
+};
 
 export default pollService;
